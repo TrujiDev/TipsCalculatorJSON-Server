b79703a5372af05977ffcdd9d9fa5b3119ef4737
TrujiDev <eetgrisales@gmail.com>
2023-11-22 14:52:06 -0500

    Add showFoodPlates function to display food plates in the UI

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let customer = {
 	order: [],
 };
 
+const categories = {
+    1: 'meal',
+    2: 'Drink',
+    3: 'Dessert',
+}
+
 const btnSaveCustomer = document.querySelector('#saveCustomer');
 btnSaveCustomer.addEventListener('click', saveCustomer);
 
@@ -50,4 +56,33 @@ function getFoodPlates() {
         .then(response => response.json())
         .then(data => showFoodPlates(data))
         .catch(error => console.log(error));
+}
+
+function showFoodPlates(foodPlates) { 
+    const content = document.querySelector('#foodPlates .content');
+
+    foodPlates.forEach(foodPlate => { 
+        const { id, name, price, category } = foodPlate;
+
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const foodPlateName = document.createElement('DIV');
+        foodPlateName.classList.add('col-md-4',);
+        foodPlateName.textContent = name;
+
+        const foodPlatePrice = document.createElement('DIV');
+        foodPlatePrice.classList.add('col-md-3', 'fw-bold');
+        foodPlatePrice.textContent = `$${price}`;
+
+        const foodPlateCategory = document.createElement('DIV');
+        foodPlateCategory.classList.add('col-md-3');
+        foodPlateCategory.textContent = categories[category];
+
+        row.appendChild(foodPlateName);
+        row.appendChild(foodPlatePrice);
+        row.appendChild(foodPlateCategory);
+
+        content.appendChild(row);
+    });
 }
