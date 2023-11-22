05272c6c947532c4ddfaa6f7abf0e2e1c49f1cd3
TrujiDev <eetgrisales@gmail.com>
2023-11-22 14:31:00 -0500

    Refactor saveCustomer function and add showSections and getFoodPlates functions

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 let customer = {
 	table: '',
 	time: '',
-	order: '',
+	order: [],
 };
 
-const btnSaveCustomer = document.getElementById('saveCustomer');
+const btnSaveCustomer = document.querySelector('#saveCustomer');
 btnSaveCustomer.addEventListener('click', saveCustomer);
 
 function saveCustomer() {
-    const table = document.getElementById('table');
-    const time = document.getElementById('time');
+	const table = document.querySelector('#table').value;
+	const time = document.querySelector('#time').value;
 
-    const emptyFields = [table, time].some(field => field.value === '');
+	const emptyFields = [table, time].some(field => field === '');
 
-    if (emptyFields) {
-        console.log('At least one field is empty');
-    } else {
-        console.log('All fields are filled');
-    }
+	if (emptyFields) {
+		const alertExist = document.querySelector('.invalid-feedback');
+
+		if (!alertExist) {
+			const alert = document.createElement('DIV');
+			alert.classList.add('invalid-feedback', 'd-block', 'text-center');
+			alert.textContent = 'All fields are required';
+			document.querySelector('.modal-body form').appendChild(alert);
+
+			setTimeout(() => {
+				alert.remove();
+			}, 3000);
+		}
+		return;
+	}
+	customer = { ...customer, table, time };
+
+	const modalForm = document.querySelector('#form');
+	const modal = bootstrap.Modal.getInstance(modalForm);
+	modal.hide();
+
+	showSections();
+	getFoodPlates();
+}
+
+function showSections() {
+	const hideSections = document.querySelectorAll('.d-none');
+	hideSections.forEach(section => section.classList.remove('d-none'));
+}
+
+function getFoodPlates() {
+    const url = 'http://localhost:3000/foodPlates';
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showFoodPlates(data))
+        .catch(error => console.log(error));
 }
